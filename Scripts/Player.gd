b83c0383ab1a6e2733a8d0f853d5d38b1e7340f7
Wholffe <marc2002sch@googlemail.com>
2023-11-15 22:58:45 +0100

    create AnimationTree, change animation player in player script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,23 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_attacking = false
 var toggle_attack = false
 var can_jump = false
+var direction: Vector2 = Vector2.ZERO
 var dir = 1
 
+@onready var animation_tree: AnimationTree = $AnimationTree
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var animation: AnimationPlayer = $AnimationPlayer
+
 @onready var coyote_timer: Timer = $coyote_timer
 @onready var jump_buffer: Timer = $jump_buffer
+
+func _ready():
+	animation_tree.active = true
 	
 func _physics_process(delta):
 	if not is_on_floor():
@@ -22,31 +25,23 @@ func _physics_process(delta):
 	else:
 		can_jump = true
 		
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-		if direction > 0:
-			sprite.flip_h = false
-			dir = 1
-		else:
-			sprite.flip_h = true
-			dir = -1
+	direction = Input.get_vector("left","right","up","down")
+	if direction.x != 0:
+		velocity.x = direction.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if(!is_attacking):
-		update_animation()
 		check_attack()
-	
 	if !jump_buffer.is_stopped() and (is_on_floor() or !coyote_timer.is_stopped() and can_jump):
 		jump()
-
 	if Input.is_action_just_pressed("jump"):
 		jump_buffer.start()
-		
 	var was_on_floor = is_on_floor()
 	
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
 	
 	if was_on_floor and !is_on_floor() and coyote_timer.is_stopped():
 		coyote_timer.start()
@@ -56,30 +51,26 @@ func jump():
 	can_jump = false
 	
 func update_animation():
-	if not is_on_floor():
-		if(velocity.y > 0):
-			animation.play("fall")
-		else:
-			animation.play("jump")
-	else:
-		if(velocity.x == 0): 
-			animation.play("idle")
-		else:
-			animation.play("run")
+	animation_tree.set("parameters/move/blend_position",direction.x)
+	
+func update_facing_direction():
+	if direction.x > 0:
+		sprite.flip_h = false
+	elif direction.x < 0:
+		sprite.flip_h = true
 	
 func check_attack():
 	if Input.is_action_just_pressed("attack"):
 		is_attacking = true
-		animation.play("attack")
 		await get_tree().create_timer(0.2).timeout
 		$Area2D/CollisionShape2D.disabled = false
 		if(sprite.flip_h):
 			$Area2D/CollisionShape2D.position.x *= -1
 			toggle_attack = true
 
-func _on_animation_player_animation_finished(anim_name):
-	is_attacking = false
-	$Area2D/CollisionShape2D.disabled = true
-	if toggle_attack:
-		$Area2D/CollisionShape2D.position.x *= -1
-		toggle_attack = false
+#func _on_animation_player_animation_finished(anim_name):
+#	is_attacking = false
+#	$Area2D/CollisionShape2D.disabled = true
+#	if toggle_attack:
+#		$Area2D/CollisionShape2D.position.x *= -1
+#		toggle_attack = false
